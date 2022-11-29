de6495dded0e81e99bff800435276984af516684
Thibisault <72867272+Thibisault@users.noreply.github.com>
2022-11-29 07:29:32 +0100

    first commit

---
+++ src/main/java/com/example/Sprint1ManagePatient/Sprint1ManagePatientApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sprint1ManagePatient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprint1ManagePatientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprint1ManagePatientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Sprint1ManagePatient/Sprint1ManagePatientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sprint1ManagePatient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprint1ManagePatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
